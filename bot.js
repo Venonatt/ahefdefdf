338132dca302bd248a7d9b6fe4733a6fad78233a
Venonatt <42704102+Venonatt@users.noreply.github.com>
2018-09-12 22:20:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1590,9 +1590,9 @@ return;
         message.guild.members.forEach(m => {
    if(!message.member.hasPermission('ADMINISTRATOR')) return;
             var bc = new Discord.RichEmbed()
-            .addField('» السيرفر :', `${message.guild.name}`)
-            .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
-            .addField(' » الرسالة : ', args)
+            .addField('**» السيرفر :**', `${message.guild.name}`)
+            .addField('**» المرسل : **', `${message.author.username}#${message.author.discriminator}`)
+            .addField('** » الرسالة : **', args)
             .setColor('#ff0000')
             // m.send(`[${m}]`);
             m.send(`${m}`,{embed: bc});
